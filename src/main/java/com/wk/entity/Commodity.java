0b266a2a99db2ea75abbc725827a933e6c1827c4
xwk-dev <2774225452@qq.com>
2021-07-19 16:31:47 +0800

    代码自动生成之后 7.19

---
+++ src/main/java/com/wk/entity/Commodity.java
@@ -0,0 +1,46 @@
+package com.wk.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.Version;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 哇卡哇卡
+ * @since 2021-07-19
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="Commodity对象", description="")
+public class Commodity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "商品id")
+    @TableId(value = "c_id", type = IdType.AUTO)
+    private Integer cId;
+
+    @ApiModelProperty(value = "商品名称")
+    private String cName;
+
+    @ApiModelProperty(value = "商品类型")
+    private String cType;
+
+    @ApiModelProperty(value = "商品数量")
+    private Integer cNum;
+
+    @ApiModelProperty(value = "商品价格")
+    private Integer cPrice;
+
+
+}
---
+++ src/main/java/com/wk/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.wk.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wk.entity.Order;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 哇卡哇卡
+ * @since 2021-07-19
+ */
+public interface OrderService extends IService<Order> {
+
+}
