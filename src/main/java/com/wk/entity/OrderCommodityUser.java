132f677423210889253628caf9caa6630e48b54c
li <893155702@qq.com>
2021-07-21 17:13:24 +0800

    增加订单sql语句和实体构造器

--- src/main/java/com/wk/entity/OrderCommodityUser.java
+++ src/main/java/com/wk/entity/OrderCommodityUser.java
@@ -1,12 +1,16 @@
 package com.wk.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Makonike
  * @date 2021-07-20 12:58
  **/
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderCommodityUser extends Order{
 
     private Commodity commodity;
