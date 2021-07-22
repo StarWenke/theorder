bba6ab2a4867c63576c54dc806373a554385ad6d
Makonike <1694585033@qq.com>
2021-07-23 00:27:07 +0800

    新增sql文件

--- sql/theorder.sql
+++ sql/theorder.sql
@@ -1,3 +1,19 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Makonike
+ Source Server Type    : MySQL
+ Source Server Version : 50733
+ Source Host           : localhost:3306
+ Source Schema         : theorder
+
+ Target Server Type    : MySQL
+ Target Server Version : 50733
+ File Encoding         : 65001
+
+ Date: 23/07/2021 00:19:40
+*/
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -7,18 +23,20 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `commodity`;
 CREATE TABLE `commodity`  (
   `c_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品id',
-  `c_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '无' COMMENT '商品名称',
-  `c_type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '无' COMMENT '商品类型',
+  `c_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '无' COMMENT '商品名称',
+  `c_type` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '无' COMMENT '商品类型',
+  `c_price` decimal(10, 2) NOT NULL COMMENT '商品价格',
   `c_num` int(10) NOT NULL COMMENT '商品数量',
-  `c_price` int(10) NOT NULL COMMENT '商品价格',
   PRIMARY KEY (`c_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of commodity
 -- ----------------------------
-INSERT INTO `commodity` VALUES (1, '多用挂篮', '家用', 1, 123);
-INSERT INTO `commodity` VALUES (2, 'R7000P', '电子设备', 1, 3220);
+INSERT INTO `commodity` VALUES (1, 'R7000P', '电子产品', 7000.00, 1);
+INSERT INTO `commodity` VALUES (2, '多用挂篮', '生活用品', 25.00, 2);
+INSERT INTO `commodity` VALUES (3, '数据结构', '书籍', 30.00, 1);
+INSERT INTO `commodity` VALUES (4, '新华字典', '书籍', 35.00, 1);
 
 -- ----------------------------
 -- Table structure for order
@@ -26,38 +44,40 @@ INSERT INTO `commodity` VALUES (2, 'R7000P', '电子设备', 1, 3220);
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order`  (
   `o_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '订单id',
-  `o_createTime` datetime(0) NULL DEFAULT NULL,
-  `o_user` int(11) NOT NULL COMMENT '下单的用户',
-  `o_commodity` int(11) NOT NULL COMMENT '下单的商品',
-  `o_condition` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
-  PRIMARY KEY (`o_id`) USING BTREE,
-  INDEX `o_user`(`o_user`) USING BTREE,
-  INDEX `o_commodity`(`o_commodity`) USING BTREE,
-  CONSTRAINT `o_commodity` FOREIGN KEY (`o_commodity`) REFERENCES `commodity` (`c_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `o_user` FOREIGN KEY (`o_user`) REFERENCES `user` (`u_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `o_no` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单编号',
+  `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  `status` tinyint(3) NOT NULL DEFAULT 0 COMMENT '订单状态',
+  `u_id` int(11) NULL DEFAULT NULL,
+  `c_id` int(11) NULL DEFAULT NULL,
+  `total_amount` decimal(10, 2) NOT NULL COMMENT '订单总金额',
+  PRIMARY KEY (`o_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of order
 -- ----------------------------
-INSERT INTO `order` VALUES (1, '2021-07-18 23:46:20', 1, 1, '0');
-INSERT INTO `order` VALUES (2, '2021-07-20 14:04:16', 1, 2, '0');
+INSERT INTO `order` VALUES (1, '202122220011', '2021-07-23 00:13:05', '2021-07-23 00:14:14', 0, 1, 1, 7000.00);
+INSERT INTO `order` VALUES (2, '202122220012', '2021-07-23 00:16:03', '2021-07-23 00:16:10', 0, 1, 2, 50.00);
+INSERT INTO `order` VALUES (3, '202122220013', '2021-07-23 00:18:54', '2021-07-23 00:19:00', 0, 2, 3, 30.00);
+INSERT INTO `order` VALUES (4, '202122220014', '2021-07-23 00:19:16', '2021-07-23 00:19:16', 0, 2, 4, 35.00);
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `u_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `u_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '无' COMMENT '用户名称',
-  `u_phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户手机号',
+  `u_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `u_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '无' COMMENT '用户名称',
+  `u_phone` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户手机号',
   `u_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户收货地址',
   PRIMARY KEY (`u_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, '吕程风', '13135555404', 'gdut');
+INSERT INTO `user` VALUES (2, '王明明', '13595555589', 'gdut');
 
 SET FOREIGN_KEY_CHECKS = 1;
